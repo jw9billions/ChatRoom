f5bee5be2d3bfbe444600893dbd2caf96af66ddd
JANE SWEET <jane@JANEs-Air.attlocal.net>
2017-05-05 23:48:20 -0700

    cp5

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,25 @@
 (function() {
-    function HomeCtrl($scope, Room) {
-		this.rooms = Room.all;
+    function HomeCtrl(Room, Message, $scope) {
+        rooms = Room.all;
+
+        rooms.$loaded().then(function(rooms) {
+            var key1 = '<KEY_FIRST_ROOM';
+            room = rooms.$getRecord(key1);
+
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
+        });
+
+        this.rooms = rooms;
+
+        this.changeRoom = function(room) {
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
+        };
+
+        this.send = Message.send;
     }
+
 
     angular
         .module('blocChat')
